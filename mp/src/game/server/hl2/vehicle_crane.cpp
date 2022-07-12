0b965f7a8df4dbae29e8f664f64dd476db3bbf3a
Potato Lover <67855370+weaponcubemap@users.noreply.github.com>
2022-07-13 00:49:52 +0300

    Fixed crashes when you remove crane_tip

--- mp/src/game/server/hl2/vehicle_crane.cpp
+++ mp/src/game/server/hl2/vehicle_crane.cpp
@@ -509,6 +509,9 @@ void CPropCrane::SetupMove( CBasePlayer *player, CUserCmd *ucmd, IMoveHelper *pH
 //-----------------------------------------------------------------------------
 void CPropCrane::DriveCrane( int iDriverButtons, int iButtonsPressed, float flNPCSteering )
 {
+	if (m_hCraneTip == NULL)
+		return;
+
 	bool bWasExtending = m_bExtending;
 
 	// Handle rotation of the crane
@@ -643,6 +646,9 @@ void CPropCrane::DriveCrane( int iDriverButtons, int iButtonsPressed, float flNP
 //-----------------------------------------------------------------------------
 void CPropCrane::RecalculateCraneTip( void )
 {
+	if (m_hCraneTip == NULL)
+		return;
+
 	Vector vecOrigin;
 	QAngle vecAngles;
 	GetCraneTipPosition( &vecOrigin, &vecAngles );
@@ -662,7 +668,7 @@ void CPropCrane::RecalculateCraneTip( void )
 //-----------------------------------------------------------------------------
 void CPropCrane::RunCraneMovement( float flTime )
 {
-	if (m_hCraneMagnet == NULL)
+	if (m_hCraneMagnet == NULL || m_hCraneTip == NULL)
 		return;
 
 	if ( m_flExtensionRate )
