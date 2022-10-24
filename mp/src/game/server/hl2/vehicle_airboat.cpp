5af6bb067264a509a3bc79b7fa0d39ab3f4af589
Potato Lover <67855370+weaponcubemap@users.noreply.github.com>
2022-10-24 14:23:33 +0300

    Fixed bug, when barnacle grabs airboat driver with flashlight on, it won't turn off

--- mp/src/game/server/hl2/vehicle_airboat.cpp
+++ mp/src/game/server/hl2/vehicle_airboat.cpp
@@ -749,6 +749,11 @@ void CPropAirboat::ExitVehicle( int nRole )
 	controller.SoundChangeVolume( m_pWaterStoppedSound, 0.0, 0.0 );
 	controller.SoundChangeVolume( m_pWaterFastSound, 0.0, 0.0 );
 	controller.SoundChangeVolume( m_pGunFiringSound, 0.0, 0.0 );
+
+	if (m_bHeadlightIsOn)
+	{
+		HeadlightTurnOff();
+	}
 }
 
 
