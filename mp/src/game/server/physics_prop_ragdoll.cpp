35bb7a018a82111dc0370e55839bf5735e1ac573
Potato Lover <67855370+weaponcubemap@users.noreply.github.com>
2023-03-23 17:17:19 +0200

    Fixed a bug when you spawn prop_ragdoll without bones, it crashes

--- mp/src/game/server/physics_prop_ragdoll.cpp
+++ mp/src/game/server/physics_prop_ragdoll.cpp
@@ -920,6 +920,9 @@ bool CRagdollProp::TestCollision( const Ray_t &ray, unsigned int mask, trace_t& 
 
 void CRagdollProp::Teleport( const Vector *newPosition, const QAngle *newAngles, const Vector *newVelocity )
 {
+	if (!m_ragdoll.list[0].pObject)
+		return;
+
 	// newAngles is a relative transform for the entity
 	// But a ragdoll entity has identity orientation by design
 	// so we compute a relative transform here based on the previous transform
