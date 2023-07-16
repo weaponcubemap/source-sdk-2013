29b02a2d890c44d3292ea50c827597f2a1acbd71
Potato Lover <67855370+weaponcubemap@users.noreply.github.com>
2023-07-17 00:14:52 +0300

    The scanner battery will no longer respawn

--- mp/src/game/server/hl2/npc_scanner.cpp
+++ mp/src/game/server/hl2/npc_scanner.cpp
@@ -367,6 +367,7 @@ void CNPC_CScanner::Gib( void )
 			pBattery->SetAbsVelocity( GetAbsVelocity() );
 			pBattery->SetLocalAngularVelocity( GetLocalAngularVelocity() );
 			pBattery->ActivateWhenAtRest();
+			pBattery->AddSpawnFlags(SF_NORESPAWN);
 			pBattery->Spawn();
 		}
 	}
