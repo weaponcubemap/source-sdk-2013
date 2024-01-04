0e1a5d1f3d10b5604414e0a5adafa4ca98cdd39d
Potato Lover <67855370+weaponcubemap@users.noreply.github.com>
2024-01-04 22:48:11 +0200

    Fixed weapon_slam not being removed after being dropped by a player

--- mp/src/game/shared/hl2mp/weapon_slam.cpp
+++ mp/src/game/shared/hl2mp/weapon_slam.cpp
@@ -145,6 +145,12 @@ void CWeapon_SLAM::Precache( void )
 void CWeapon_SLAM::SetPickupTouch( void )
 {
 	SetTouch(&CWeapon_SLAM::SlamTouch);
+
+	if (GetSpawnFlags() & SF_NORESPAWN)
+	{
+		SetThink(&CWeapon_SLAM::SUB_Remove);
+		SetNextThink(gpGlobals->curtime + 30.0f);
+	}
 }
 
 //-----------------------------------------------------------------------------
